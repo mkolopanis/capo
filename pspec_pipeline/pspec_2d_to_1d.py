018a9804741c8526b3f8d2d871a7ea06c0df35c6
Carina Cheng <cacheng@folio.sas.upenn.edu>
2017-01-19 17:54:15 -0500

    cleaned up script by removing commented out things

--- pspec_pipeline/pspec_2d_to_1d.py
+++ pspec_pipeline/pspec_2d_to_1d.py
@@ -20,23 +20,19 @@ parser = argparse.ArgumentParser(
                          'from pspec_oqe_2d.py'))
 parser.add_argument('files', metavar='<FILE>', type=str, nargs='+',
                     help='List of files to average')
-# parser.add_argument('--bl_length', type=float,  required=True,
-#                    help='length of baseline in meters')
 parser.add_argument('--sub_pCv', action='store_true',
                     help='Remove pCv back from pC before averaging')
 parser.add_argument('--output', type=str, default='./',
                     help='Specifically specify out directory.')
 parser.add_argument('--nboots', type=int, default=100,
                     help='Number of Bootstraps (averages) default=100')
-# parser.add_argument('--mode', dest='mode', choices=['prob','excess'],
-#                     default='prob',
-#                     help='limit estimation method')
 args = parser.parse_args(sys.argv[1:])
 
 pspecs = read_bootstraps_dcj(args.files)
 Nlstbins = np.shape(pspecs['pCr'])[-1]
 # get the number of lst integrations in the dataset
-t_eff = pspecs['frf_inttime']/pspecs['inttime']
+try: t_eff = pspecs['frf_inttime']/pspecs['inttime']
+except: t_eff = 3887./32. #XXX PSA128
 Neff_lst = np.ceil(Nlstbins/t_eff)
 # compute the effective number of LST bins
 # print Neff_lst
@@ -61,19 +57,9 @@ bl_length = np.linalg.norm(pspecs['uvw'])
 wavelength = cosmo_units.c/(pspecs['freq']*1e9)
 ubl = bl_length/wavelength
 kperp = dk_du(pspecs['freq'])*ubl
-print bl_length
 print "freq = ", pspecs['freq']
 print "kperp = ", kperp
 pk_pspecs['k'] = np.sqrt(kperp**2 + pk_pspecs['kpl_fold']**2)
-# apply corrections to all the various channels
-pspec_channels = ['pCr', 'pIr', 'pCv', 'pIv']
-corrections = [1/np.sqrt(2),  # the median overestimates by sqrt(2)
-               1.39]        # beam^2
-# for chan in pspec_channels:
-#     for c in [chan,chan+'_fold']: #no need to correct error bars
-#         for correction in corrections:
-#             pk_pspecs[c] *= correction
-# make a pspec file
 for key in pk_pspecs.keys():
     if pk_pspecs[key].dtype not in [np.float]:
         continue
