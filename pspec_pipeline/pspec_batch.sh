06aa4629b46b4011158705fd110544fc7ccddade
Carina Cheng <cacheng@folio.sas.upenn.edu>
2017-01-18 19:04:46 -0500

    -batch script calls pspec_oqe_2d and pspec_2d_to_1d -inputs given to the script are the path to LST-binned files, and a directory name to throw outputs into

--- pspec_pipeline/pspec_batch.sh
+++ pspec_pipeline/pspec_batch.sh
@@ -1,35 +1,38 @@
-### My Data ###
-PREFIX='/data4/paper/2013EoR/Analysis/ProcessedData/epoch2/omni_v3_xtalk/lstbin/'
-EVEN_FILES=${PREFIX}'even/sep0,2/*I.uvGAL'
-ODD_FILES=${PREFIX}'odd/sep0,2/*I.uvGAL'
+### Sample Call ###
+#   $ pspec_batch.sh <path to LST-binned files> <directory name to save all outputs>#   ex: pspec_batch.sh /data4/paper/2013EoR/Analysis/ProcessedData/epoch2/omni_v3_xtalk/lstbin pspec_jan2017
+
+### My Paths ###
+DATA=$1
+EVEN_FILES=${DATA}'/even/sep0,2/*I.uvGAL'
+ODD_FILES=${DATA}'/odd/sep0,2/*I.uvGAL'
+DIRNAME=$2
 
 ### My Options ###
-RA='4_10' #'1_10'
-EVEN_FILES=`lst_select.py -C psa6622_v003 --ra=${RA} ${EVEN_FILES[@]}`
-ODD_FILES=`lst_select.py -C psa6622_v003 --ra=${RA} ${ODD_FILES[@]}`
-SEP='0,2'
+RA='4_10'
 CALFILE='psa6622_v003'
+EVEN_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${EVEN_FILES[@]}`
+ODD_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${ODD_FILES[@]}`
+SEP='0,2'
 CHAN='79_99'
 NBOOT=20
 POL='I'
 WINDOW='none'
-DIRNAME='TEST_pspec_'${SEP}'_'${CHAN}'_'${RA}'_'${POL}
 FRFEOR='--frfeor' #to FRF the injected EOR, leave this on
+SUBPCV='--sub_pCv' #to subtract pCv before bootstrapping, leave this on
 
 #-----------------------------------------------------------------
 
 # Make Power Spectrum Directory
 mkdir ${DIRNAME}
 echo Making Directory ${DIRNAME}
-cd ${DIRNAME}
 
 # Stage 1: pspec_oqe_2d.py over range of injection levels
 for inject in `python -c "import numpy; print ' '.join(map(str, numpy.logspace(-1,3,10)))"` ; do
-    mkdir inject_sep${SEP}_${inject}
+    mkdir ${DIRNAME}/inject_sep${SEP}_${inject}
     echo SIGNAL_LEVEL=${inject}
-    ~/capo/pspec_pipeline/pspec_oqe_2d.py --window=${WINDOW} -a cross -p ${POL} -c ${CHAN} -C ${CALFILE} -b ${NBOOT} ${FRFEOR} -i ${inject} ${EVEN_FILES} ${ODD_FILES}
-    mv *bootsigloss*.npz inject_sep${SEP}_${inject}/.
+    ~/capo/pspec_pipeline/pspec_oqe_2d.py --window=${WINDOW} -a cross -p ${POL} -c ${CHAN} -C ${CALFILE} -b ${NBOOT} ${FRFEOR} -i ${inject} --output ${DIRNAME}/inject_sep${SEP}_${inject} ${EVEN_FILES} ${ODD_FILES}
 done
+    
+    # Stage 2: pspec_2d_to_1d.py 
+    ~/capo/pspec_pipeline/pspec_2d_to_1d.py ${SUBPCV} --output ${DIRNAME} ${DIRNAME}/inject_sep${SEP}_${inject}/*boot*
 
-# Stage 2: pspec_2d_to_1d.py 
-~/capo/pspec_pipeline/pspec_2d_to_1d.py 
