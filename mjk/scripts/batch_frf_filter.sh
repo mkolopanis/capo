b06ca0dbd387b0d3edebe5a38a8f392255634811
mkolopanis <matthew.kolopanis@gmail.com>
2017-01-20 10:51:15 -0700

    batch_frf_filter takes input and output directories

--- mjk/scripts/batch_frf_filter.sh
+++ mjk/scripts/batch_frf_filter.sh
@@ -2,9 +2,11 @@
 
 seps='sep0,1 sep1,1 sep-1,1'
 cal=psa6240_v003
-out='/data3/PAPER/psa64/exp_vs_inttime/lstbin_psa64_data_optimal_20Jan2017'
 appelation='uvGA'
 chan='101'
+
+indir=$1
+outdir=$2
 
 scriptsdir=/home/djacobs/scripts
 
@@ -21,12 +23,12 @@ printf 'This is Batch Fringe Rate Filter using:\n'
 printf 'calfile: %s \n' $cal
 sleep 1.5
 
-if [ ! -d $out   ]; then
+if [ ! -d $outdir   ]; then
     printf 'This output director appears to be new\n'
     printf 'Creating Directoires now\n'
     for path in $days; do
         for sep in $seps; do
-            mkdir -p ${out}/${path}/${sep}
+            mkdir -p ${outdir}/${path}/${sep}
         done
    done
 fi
@@ -34,19 +36,19 @@ fi
 for path in $days; do
     for sep in $seps; do
         printf 'Checking for Old FRF Data\n'
-        outpath=$out/$path
-        if [[ $(ls -d $outpath/$sep/*.${appelation}L) ]] ; then
-            printf 'Found %s folders\n' $(ls -d $outpath/$sep/*.${appelation}L| wc -l)
+        outpath=$outdir/$path/$sep
+        if [[ $(ls -d $outpath/*.${appelation}L) ]] ; then
+            printf 'Found %s folders\n' $(ls -d $outpath/*.${appelation}L| wc -l)
             printf 'First deleting old FRF Filtered Data \n'
             sleep 1
             printf 'I wil delete: \n'
             sleep 1
-            printf '%s \n' $(ls -d $outpath/$sep/*.${appelation}L)
+            printf '%s \n' $(ls -d $outpath/*.${appelation}L)
             read -p "Are you sure you want to delete these? [y/N] " -n 1 -r
             printf '\n'
             if [[ $REPLY =~ ^[Yy]$ ]]
                 then
-                    rm -rf $(ls -d $outpath/$sep/*.${appelation}L)
+                    rm -rf $(ls -d $outpath/*.${appelation}L)
                     printf 'Files are deleted\nContinuting to Fringe Rate Filter\n'
                 else
                     printf 'Nothing is deleted\ncontinuing\n'
@@ -54,22 +56,22 @@ for path in $days; do
             fi
         fi
 
-        files=$(ls -d $path/$sep/*.${appelation})
+        files=$(ls -d $indir/$path/$sep/*.${appelation})
         printf 'Filtering %s by selecting ant %s \n' $sep ${ants[$sep]}
 
         if ${alietal}; then
 
             printf '%s/frf_filter.py -C %s  --alietal -a %s -c %s  --outpath=%s/' \
-            $scriptsdir  $cal ${ants[$sep]} $chan h ${out}
+            $scriptsdir  $cal ${ants[$sep]} $chan h ${outpath}
             printf '%s\n' $path/$sep
             "${scriptsdir}/frf_filter.py" -C $cal -a ${ants[$sep]} -C $cal --alietal \
-            $files --outpath=${out} -c $chan
+            $files --outpath=${outpath} -c $chan
 
         else
-            printf '%s/frf_filter.py -C %s   -a %s -c %s --outpath=%s/' $scriptsdir $cal ${ants[$sep]} $chan ${out}
+            printf '%s/frf_filter.py -C %s   -a %s -c %s --outpath=%s/' $scriptsdir $cal ${ants[$sep]} $chan ${outpath}
             printf '%s\n' $path/$sep
             "${scriptsdir}/frf_filter.py" -C $cal -a ${ants[$sep]} -C $cal\
-            $files --outpath=${out} -c $chan --pol=I
+            $files --outpath=${outpath} -c $chan --pol=I
 
         fi
     done
