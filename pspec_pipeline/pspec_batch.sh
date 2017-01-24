94f9cebf4eee54e63ba70888bc99af3077076197
Carina Cheng <cacheng@folio.sas.upenn.edu>
2017-01-24 16:09:19 -0500

    removed frfeor option from pspec_batch

--- pspec_pipeline/pspec_batch.sh
+++ pspec_pipeline/pspec_batch.sh
@@ -10,7 +10,7 @@ ODD_FILES=${DATA}'/odd/sep0,2/*I.uvGAL'
 DIRNAME=$2
 
 ### My Options ###
-RA='4_10'
+RA='1_10'
 CALFILE='psa6622_v003'
 EVEN_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${EVEN_FILES[@]}`
 ODD_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${ODD_FILES[@]}`
@@ -20,7 +20,6 @@ NBOOT=20
 POL='I'
 weight='L^-1'
 WINDOW='none'
-FRFEOR='--frfeor' #to FRF the injected EOR, leave this on
 
 #-----------------------------------------------------------------
 
@@ -32,7 +31,7 @@ echo Making Directory ${DIRNAME}
 for inject in `python -c "import numpy; print ' '.join(map(str, numpy.logspace(-2,3,10)))"` ; do
     mkdir ${DIRNAME}/inject_sep${SEP}_${inject}
     echo SIGNAL_LEVEL=${inject}
-    ~/capo/pspec_pipeline/pspec_oqe_2d.py --window=${WINDOW} -a cross -p ${POL} -c ${CHAN} -C ${CALFILE} -b ${NBOOT} ${FRFEOR} -i ${inject} --weight=${weight} --output ${DIRNAME}/inject_sep${SEP}_${inject} ${EVEN_FILES} ${ODD_FILES}
+    ~/capo/pspec_pipeline/pspec_oqe_2d.py --window=${WINDOW} -a cross -p ${POL} -c ${CHAN} -C ${CALFILE} -b ${NBOOT} -i ${inject} --weight=${weight} --output ${DIRNAME}/inject_sep${SEP}_${inject} ${EVEN_FILES} ${ODD_FILES}
 
     # Stage 2: pspec_2d_to_1d.py
     ~/capo/pspec_pipeline/pspec_2d_to_1d.py --output ${DIRNAME}/inject_sep${SEP}_${inject}/ ${DIRNAME}/inject_sep${SEP}_${inject}/*boot*
