cfc6c17ffd2b52949137d8693fc0a4ccbb410a82
Matthew Kolopanis <matthew.kolopanis@gmail.com>
2017-01-30 12:47:13 -0700

    setup pspec_batch for 3 seps and averaging in psa64

--- pspec_pipeline/pspec_batch.sh
+++ pspec_pipeline/pspec_batch.sh
@@ -5,16 +5,12 @@
 
 ### My Paths ###
 DATA=$1
-EVEN_FILES=${DATA}'/even/sep0,2/*I.uvGAL'
-ODD_FILES=${DATA}'/odd/sep0,2/*I.uvGAL'
 DIRNAME=$2
 
 ### My Options ###
 RA='1_10'
 CALFILE='psa6622_v003'
-EVEN_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${EVEN_FILES[@]}`
-ODD_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${ODD_FILES[@]}`
-SEP='0,2'
+SEP='0,1 1,1 -1,1'
 CHAN='79_99'
 NBOOT=20
 POL='I'
@@ -36,12 +32,27 @@ FRF='--frf'
 mkdir ${DIRNAME}
 echo Making Directory ${DIRNAME}
 
-# Stage 1: pspec_oqe_2d.py over range of injection levels
-for inject in `python -c "import numpy; print ' '.join(map(str, numpy.logspace(-2,3,10)))"` ; do
-#    mkdir ${DIRNAME}/inject_sep${SEP}_${inject}
-#    echo SIGNAL_LEVEL=${inject}
-#    ~/capo/pspec_pipeline/pspec_oqe_2d.py --window=${WINDOW} -a cross -p ${POL} -c ${CHAN} -C ${CALFILE} -b ${NBOOT} -i ${inject} --weight=${weight} ${FRF} --output ${DIRNAME}/inject_sep${SEP}_${inject} ${EVEN_FILES} ${ODD_FILES}
-
-    # Stage 2: pspec_2d_to_1d.py
-    ~/capo/pspec_pipeline/pspec_2d_to_1d.py --output ${DIRNAME}/inject_sep${SEP}_${inject}/ ${DIRNAME}/inject_sep${SEP}_${inject}/*boot*
+for sep in $SEP; do
+    continue
+    mkdir ${DIRNAME}/sep${sep}
+    EVEN_FILES=${DATA}'/even/sep'${sep}'/*I.uvGAL'
+    ODD_FILES=${DATA}'/odd/sep'${sep}'/*I.uvGAL'
+    EVEN_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${EVEN_FILES[@]}`
+    ODD_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${ODD_FILES[@]}`
+    # Stage 1: pspec_oqe_2d.py over range of injection levels
+    for inject in `python -c "import numpy; print ' '.join(map(str, numpy.logspace(-2,4,10)))"` ; do
+        mkdir ${DIRNAME}/sep${sep}/inject_sep${SEP}_${inject}
+        echo SIGNAL_LEVEL=${inject}
+        ~/capo/pspec_pipeline/pspec_oqe_2d.py --window=${WINDOW} -a cross -p ${POL} -c ${CHAN} -C ${CALFILE} -b ${NBOOT} -i ${inject} --weight=${weight} --output ${DIRNAME}/sep${sep}/inject_sep${sep}_${inject} ${EVEN_FILES} ${ODD_FILES}
+    
+        # Stage 2: pspec_2d_to_1d.py
+        ~/capo/pspec_pipeline/pspec_2d_to_1d.py --output ${DIRNAME}/sep${sep}/inject_sep${sep}_${inject}/ ${DIRNAME}/sep${sep}/inject_sep${sep}_${inject}/*boot*
+    done
 done
+
+for inject in `python -c "import numpy; print ' '.join(map(str, numpy.logspace(-2,4,10)))"` ; do
+    mkdir ${DIRNAME}/inject_${inject}
+    python ~/src/capo/pspec_pipeline/pspec_average_seps.py --outfile ${DIRNAME}/inject_${inject}/psepc_pk_k3pk.npz ${DIRNAME}/sep*/inject_sep*_${inject}/pspec_pk_k3pk.npz
+done
+
+python ~/src/capo/pspec_pipeline/pspec_final_confidence.py inject_*/pspec_pk_k3pk.npz --outfile ${DIRNAME}
