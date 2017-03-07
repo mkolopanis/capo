9b96e1e52033f437bd63fed35da2ad6735694528
Carina Cheng <cacheng@folio.sas.upenn.edu>
2017-03-07 18:05:40 -0500

    -make_eor function returns both FRF(data) and FRF(conj(data)), so that data is conjugated if necessary before FRF. data_dict_e is then populated with one of them, depending on whether the key has conj=True or not -No conj_dict is given when doing set_data for noise or eor, since those datasets are already conjugated correctly

--- pspec_pipeline/pspec_oqe_2d.py
+++ pspec_pipeline/pspec_oqe_2d.py
@@ -120,10 +120,13 @@ def make_eor(shape):  # Create and fringe rate filter noise
     wij = n.ones(shape, dtype=bool)  # XXX flags are all true (times,freqs)
     # dij and wij are (times,freqs)
     _d = fringe_rate_filter(aa, dij, wij, ij[0], ij[1], POL, bins, fir)
+    _d_conj = fringe_rate_filter(aa, n.conj(dij), wij, ij[0], ij[1], POL, bins, fir)
     if opts.frf: # double FRF of eor
         _d = fringe_rate_filter(aa, _d, wij, ij[0], ij[1], POL, bins, fir)
+        _d_conj = fringe_rate_filter(aa, _d_conj, wij, ij[0], ij[1], POL, bins, fir)
     _d = _d[shape[0] / 4:shape[0] / 2 + shape[0] / 4, :]
-    return _d
+    _d_conj = _d_conj[shape[0] / 4:shape[0] / 2 + shape[0] / 4, :]
+    return _d, _d_conj
 
 
 def make_PS(keys, ds, grouping=True):
@@ -143,7 +146,7 @@ def make_PS(keys, ds, grouping=True):
     qI = n.zeros((nchan, nlst), dtype=n.complex)
     qC = n.zeros((nchan, nlst), dtype=n.complex)
     for k, key1 in enumerate(newkeys):
-        # print '   ',k+1,'/',len(keys)
+        #print '   ',k+1,'/',len(newkeys)
         for key2 in newkeys[k:]:
             if key1[0] == key2[0] or key1[1] == key2[1]:
                 continue  # don't do even w/even or bl w/same bl
@@ -352,6 +355,10 @@ nlst = data_dict_v[keys[0]].shape[0]
 # the lsts given is a dictionary with 'even','odd', etc.
 # but the lsts returned is one array
 
+for key in data_dict_v:
+    if conj_dict[key[1]] is True: # conjugate certain baselines for noise (doesn't really matter since it's noise, but it's for completeness)
+        data_dict_n[key] = n.conj(data_dict_n[key])
+
 # Fringe-rate filter noise
 if opts.frf:
     for key in data_dict_n:
@@ -365,7 +372,14 @@ if opts.frf:
 dsv = oqe.DataSet()  # just data
 dsv.set_data(dsets=data_dict_v, conj=conj_dict, wgts=flg_dict)
 dsn = oqe.DataSet()  # just noise
-dsn.set_data(dsets=data_dict_n, conj=conj_dict, wgts=flg_dict)
+dsn.set_data(dsets=data_dict_n, wgts=flg_dict) # no conj dict because it was already conjugated correctly 
+
+#n_to_save = {}
+#for kk in data_dict_n:
+#    n_to_save[str(kk)] = data_dict_n[kk]
+#print 'Saving Noise_Dataset.npz'
+#n.savez('Noise_Dataset.npz', **n_to_save)
+
 # Get some statistics
 if LST_STATS:
     # collect some metadata from the lst binning process
@@ -412,7 +426,6 @@ for boot in xrange(opts.nboot):
     
     # Only data
     pCv, pIv = make_PS(keys, dsv, grouping=True)
-
     # Only noise
     pCn, pIn = make_PS(keys, dsn, grouping=True)
 
@@ -420,16 +433,17 @@ for boot in xrange(opts.nboot):
     # and pCe & pIe (eor) #
     if INJECT_SIG > 0.:  # Create a fake EoR signal to inject
         print '  INJECTING SIMULATED SIGNAL @ LEVEL', INJECT_SIG
-        eor = (make_eor((nchan, nlst)) * INJECT_SIG)  # same on all baselines
+        eor,eor_conj = make_eor((nchan, nlst))  # same on all baselines
+        eor,eor_conj = eor*INJECT_SIG, eor_conj*INJECT_SIG
         data_dict_r = {}
         data_dict_e = {}
         data_dict_s = {}
         for key in data_dict_v:
             if conj_dict[key[1]] is True:
-                eorinject = n.conj(eor.copy())
+                eorinject = eor_conj #n.conj(eor.copy())
                 # conjugate eor for certain baselines
             else:
-                eorinject = eor.copy()
+                eorinject = eor #eor.copy()
             # Track eor in separate dict
             data_dict_e[key] = eorinject
             # add injected signal to data
@@ -441,10 +455,10 @@ for boot in xrange(opts.nboot):
     dsr = oqe.DataSet()  # data + eor
     dsr.set_data(dsets=data_dict_r, conj=conj_dict, wgts=flg_dict)
     dse = oqe.DataSet()  # just eor
-    dse.set_data(dsets=data_dict_e, conj=conj_dict, wgts=flg_dict)
+    dse.set_data(dsets=data_dict_e, wgts=flg_dict)
     dss = oqe.DataSet()  # noise + eor
-    dss.set_data(dsets=data_dict_s, conj=conj_dict, wgts=flg_dict)
-
+    dss.set_data(dsets=data_dict_s, wgts=flg_dict)
+    
     pCr, pIr = make_PS(keys, dsr, grouping=True)
     pCe, pIe = make_PS(keys, dse, grouping=True)
     pCs, pIs = make_PS(keys, dss, grouping=True)
