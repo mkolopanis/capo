79fd3b923655db850b4ee1fc235d10e2befc3ab2
Carina Cheng <cacheng@folio.sas.upenn.edu>
2017-03-07 21:00:49 -0500

    NPOL not hard-coded anymore

--- pspec_pipeline/pspec_oqe_2d.py
+++ pspec_pipeline/pspec_oqe_2d.py
@@ -47,6 +47,8 @@ opts, args = o.parse_args(sys.argv[1:])
 random.seed(0) # for oqe.py (eor generator)
 n.random.seed(0) # for noise generator
 POL = opts.pol
+if POL == 'xx' or POL == 'yy': NPOL = 1
+else: NPOL = 2
 LST_STATS = False
 DELAY = False
 NGPS = 5  # number of groups to break the random sampled bls into
@@ -83,7 +85,7 @@ def make_noise(d, cnt, inttime, df): #, freqs, jy2T=None):
     #    jy2T = capo.pspec.jy2T(freqs)
     Tsys = 180. * n.power(afreqs/0.18, -2.55) + opts.Trcvr  # system temp in K
     Tsys *= 1e3  # system temp in mK
-    Trms = Tsys/n.sqrt(df * 1e9 * inttime * cnt*len(days) * 2)  # convert sdf to Hz
+    Trms = Tsys/n.sqrt(df * 1e9 * inttime * cnt*len(days) * NPOL)  # convert sdf to Hz
                         # Bchan, inttime, counts (times 2 if even&odd), Npol
     Vrms = Trms#/jy2T  # jy2T is in units of mK/Jy
     # The following transposes are to create noise correlated in time not
