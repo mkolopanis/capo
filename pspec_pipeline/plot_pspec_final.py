ab8ed0f2153085bcf672d5e4b100b479c28ce391
Carina Cheng <cacheng@folio.sas.upenn.edu>
2017-03-17 13:57:47 -0400

    removed labels on grey points

--- pspec_pipeline/plot_pspec_final.py
+++ pspec_pipeline/plot_pspec_final.py
@@ -131,7 +131,6 @@ for filename in args.files:
     ax1[gs_ind].errorbar(pspec_dict['k'][neg_ind_fold], 
                          -pspec_dict['pC_fold'][neg_ind_fold],
                          pspec_dict['pC_fold_up'][neg_ind_fold], 
-                         label='pC {0:02d}%'.format(int(pspec_dict['prob']*100)), 
                          linestyle='',marker=marker, color='0.5')
     ax2[gs_ind].plot(pspec_dict['kpl'],
                      pspec_dict['pI'] + pspec_dict['pI_up'], '--', 
@@ -142,7 +141,6 @@ for filename in args.files:
                          linestyle='', marker=marker, color='black')
     ax2[gs_ind].errorbar(pspec_dict['kpl'][neg_ind], -pspec_dict['pC'][neg_ind],
                          pspec_dict['pC_up'][neg_ind], 
-                         label='pC {0:02d}%'.format(int(pspec_dict['prob']*100)), 
                          linestyle='', marker=marker, color='0.5')
     ax3[gs_ind].plot(pspec_dict['k'],
                      pspec_dict['pIn_fold'] + pspec_dict['pIn_fold_up'], '--',
@@ -155,7 +153,6 @@ for filename in args.files:
     ax3[gs_ind].errorbar(pspec_dict['k'][neg_ind_noise_fold], 
                          -pspec_dict['pCn_fold'][neg_ind_noise_fold],
                          pspec_dict['pCn_fold_up'][neg_ind_noise_fold], 
-                         label='pCn {0:02d}%'.format(int(pspec_dict['prob']*100)), 
                          linestyle='', marker=marker, color='0.5')
     ax4[gs_ind].plot(pspec_dict['kpl'],
                      pspec_dict['pIn'] + pspec_dict['pIn_up'], '--',
@@ -168,7 +165,6 @@ for filename in args.files:
     ax4[gs_ind].errorbar(pspec_dict['kpl'][neg_ind_noise], 
                          -pspec_dict['pCn'][neg_ind_noise],
                          pspec_dict['pCn_up'][neg_ind_noise], 
-                         label='pCn {0:02d}%'.format(int(pspec_dict['prob']*100)), 
                          linestyle='', marker=marker, color='0.5')
         
     if args.analytical:
@@ -183,7 +179,7 @@ for filename in args.files:
         """
         #PSA128
         inttime = 32 #3914
-        nbls = 64 #S1E1
+        nbls = 64*np.sqrt((20/2.)/25) #S1E1
         cnt_eff = 10 #S1E1
         nlsts = 9 #S1E1
         f1,f2 = 79,99
@@ -195,7 +191,7 @@ for filename in args.files:
         cnt = cnt_eff * (nlsts*60*60/inttime)**.5
         nmodes = (nseps*folding)**.5
         pol = 2
-        real = 2 
+        real = np.sqrt(2) 
         z = capo.pspec.f2z(freq)
         X2Y = capo.pspec.X2Y(z)/1e9 #h^-3 Mpc^3 / str/ Hz
         sdf = .1/203
@@ -206,12 +202,12 @@ for filename in args.files:
         scalar = X2Y * bm #* B
         #error bars minimum width. Consider them flat for P(k). Factor of 2 at the end is due to folding of kpl (root(2)) and root(2) in radiometer equation.
         #pk_noise = 2*scalar*fr_correct*( (tsys)**2 / (2*inttime*pol*real*nbls*ndays*nmodes) ) #this 2-sigma curve should encompass 95% of the points
-        pk_noise = 2*scalar*fr_correct*( (tsys)**2 / (inttime*pol*real*nbls*cnt*nmodes) ) #this 2-sigma curve should encompass 95% of the points
+        pk_noise = 3*scalar*fr_correct*( (tsys)**2 / (inttime*pol*real*nbls*cnt*nmodes) ) #this 3-sigma curve should line up with pI
         # Plot analytical noise curve on plots
-        ax1[gs_ind].plot(pspec_dict['k'],pk_noise*pspec_dict['k']**3/(2*np.pi**2),'g-',label='Analytical 2-sigma')
-        ax2[gs_ind].axhline(pk_noise,color='g',marker='_',label='Analytical 2-sigma')
-        ax3[gs_ind].plot(pspec_dict['k'],pk_noise*pspec_dict['k']**3/(2*np.pi**2),'g-',label='Analytical 2-sigma')
-        ax4[gs_ind].axhline(pk_noise,color='g',marker='_',label='Analytical 2-sigma')
+        ax1[gs_ind].plot(pspec_dict['k'],pk_noise*pspec_dict['k']**3/(2*np.pi**2),'g-',label='Analytical 3-sigma')
+        ax2[gs_ind].axhline(pk_noise,color='g',marker='_',label='Analytical 3-sigma')
+        ax3[gs_ind].plot(pspec_dict['k'],pk_noise*pspec_dict['k']**3/(2*np.pi**2),'g-',label='Analytical 3-sigma')
+        ax4[gs_ind].axhline(pk_noise,color='g',marker='_',label='Analytical 3-sigma')
 
 # set up some parameters to make the figures pretty
 for gs_ind in xrange(Nzs):
