cd8e855f268402274cc90210cbd164c256ffefb2
Carina Cheng <cacheng@folio2.sas.upenn.edu>
2017-04-05 14:39:53 -0400

    -Changed average function to be numpy.ma.average -Fixed typo (forgot parentheses somewhere)

--- src/eor_results.py
+++ src/eor_results.py
@@ -641,7 +641,7 @@ def average_bootstraps(indata, Nt_eff, avg_func=n.median, Nboots=100):
             Z = scramble_avg_bootstrap_array_v2(indata[inname], func=avg_func)
             vals[outname] = Z.data #bboots.data
             # power spectrum is the mean and std dev over scramble dimension
-            outdata[outname] = n.average(Z, axis=0)
+            outdata[outname] = n.ma.average(Z, axis=0)
             outdata[outname + '_err'] = n.std(Z, axis=0)
             # also do the folded version
             outname += '_fold'
@@ -650,7 +650,7 @@ def average_bootstraps(indata, Nt_eff, avg_func=n.median, Nboots=100):
             #                                 Nboots=Nboots)
             Z = scramble_avg_bootstrap_array_v2(X, func=avg_func)
             vals_fold[outname] = Z.data
-            outdata[outname] = n.average(Z, axis=0)
+            outdata[outname] = n.ma.average(Z, axis=0)
             outdata[outname + '_err'] = n.std(Z, axis=0)
             outdata['kpl_fold'] = kpl_fold
 
@@ -688,7 +688,7 @@ def scramble_avg_bootstrap_array_v2(X, func=n.median):
     bboots = []
     for j in range(X.shape[0]):
         times_i = n.random.choice(X.shape[-1], 1000, replace=True) #XXX 1000 is chosen arbitrarily, but any large number should converge on the same answer
-        if func == n.median: bboots.append(pspec_median(X[j, :, times_i], axis=0)
+        if func == n.median: bboots.append(pspec_median(X[j, :, times_i], axis=0))
         else: bboots.append(func(X[j, :, times_i], axis=0))
     bboots = n.ma.masked_invalid(n.array(bboots))
     return bboots
