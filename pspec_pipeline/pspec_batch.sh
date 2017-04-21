afa89cc298ab14088f5074554e42a8500007f84a
Carina Cheng <cacheng@folio2.sas.upenn.edu>
2017-04-21 17:43:11 -0400

    took away nboots for latest bootstrapping method

--- pspec_pipeline/pspec_batch.sh
+++ pspec_pipeline/pspec_batch.sh
@@ -5,7 +5,7 @@
 
 ### My Options ###
 CALFILE='psa6622_v003'
-RA='1_10'
+RA='4_10'
 SEP='0,2'
 DATA=$1
 EVEN_FILES=${DATA}'/even/sep'${SEP}'/*I.uvGAL'
@@ -14,7 +14,7 @@ DIRNAME=$2
 EVEN_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${EVEN_FILES[@]}`
 ODD_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${ODD_FILES[@]}`
 CHAN='110_130'
-NBOOT=20
+#NBOOT=20
 POL='I'
 weight='L^-1'
 WINDOW='none'
@@ -40,8 +40,8 @@ echo Making Directory ${DIRNAME}
 for inject in `python -c "import numpy; print ' '.join(map(str, numpy.logspace(-2,3,10)))"` ; do
     mkdir ${DIRNAME}/inject_sep${SEP}_${inject}
     echo SIGNAL_LEVEL=${inject}
-    ~/capo/pspec_pipeline/pspec_oqe_2d.py ${LMODE} ${CHANGEC} --window=${WINDOW} -a cross -p ${POL} -c ${CHAN} -C ${CALFILE} -b ${NBOOT} -i ${inject} --weight=${weight} ${FRF} --output ${DIRNAME}/inject_sep${SEP}_${inject} ${EVEN_FILES} ${ODD_FILES}
+    ~/capo/pspec_pipeline/pspec_oqe_2d.py ${LMODE} ${CHANGEC} --window=${WINDOW} -a cross -p ${POL} -c ${CHAN} -C ${CALFILE} -i ${inject} --weight=${weight} ${FRF} --output ${DIRNAME}/inject_sep${SEP}_${inject} ${EVEN_FILES} ${ODD_FILES}
 
     # Stage 2: pspec_2d_to_1d.py
-    ~/capo/pspec_pipeline/pspec_2d_to_1d.py --output ${DIRNAME}/inject_sep${SEP}_${inject}/ ${DIRNAME}/inject_sep${SEP}_${inject}/*boot*
+    ~/capo/pspec_pipeline/pspec_2d_to_1d.py --output ${DIRNAME}/inject_sep${SEP}_${inject}/ ${DIRNAME}/inject_sep${SEP}_${inject}/*pspec_oqe_2d*
 done
