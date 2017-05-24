f11a0f598e7dbbc8ba79864af0fd1558dcbe3d8f
Danny Jacobs <daniel.c.jacobs@asu.edu>
2017-05-24 09:56:21 -0700

    Updated to run for 64 too

--- pspec_pipeline/pspec_batch.sh
+++ pspec_pipeline/pspec_batch.sh
@@ -3,13 +3,37 @@
 ### Sample Call ###
 #   $ pspec_batch.sh <path to LST-binned files> <directory name to save all outputs>
 
+#PSA64
+if true
+then
+CALFILE='psa6240_v003'
+RA='.1_8.6'
+SEP='0,1'
+DATA=$1
+EVEN_FILES=${DATA}'/even/sep'${SEP}'/*.uvGA'
+ODD_FILES=${DATA}'/odd/sep'${SEP}'/*.uvGA'
+DIRNAME=$2
+EVEN_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${EVEN_FILES[@]}`
+ODD_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${ODD_FILES[@]}`
+CHAN='95_115'
+#NBOOT=20
+POL='I'
+weight='I'
+WINDOW='none'
+FRF=''
+#NOFRFPATH='--nofrfpath pspec128_uvGA/inject_sep'${SEP}'_0.01/pspec_pk_k3pk.npz' # path to one pspec_2d_to_1d.py output for NONFRF case
+NOFRFPATH=''
+LMODE='' #'--lmode=12'
+CHANGEC='' #'--changeC'
+
+else
 ### My Options ###
 CALFILE='psa6622_v003'
 RA='4_10'
 SEP='0,2'
 DATA=$1
-EVEN_FILES=${DATA}'/even/sep'${SEP}'/*I.uvGAL'
-ODD_FILES=${DATA}'/odd/sep'${SEP}'/*I.uvGAL'
+EVEN_FILES=${DATA}'/even/sep'${SEP}'/*I.uvGA'
+ODD_FILES=${DATA}'/odd/sep'${SEP}'/*I.uvGA'
 DIRNAME=$2
 EVEN_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${EVEN_FILES[@]}`
 ODD_FILES=`lst_select.py -C ${CALFILE} --ra=${RA} ${ODD_FILES[@]}`
@@ -22,7 +46,7 @@ FRF='--frf'
 NOFRFPATH='--nofrfpath pspec128_uvGA/inject_sep'${SEP}'_0.01/pspec_pk_k3pk.npz' # path to one pspec_2d_to_1d.py output for NONFRF case
 LMODE='' #'--lmode=12'
 CHANGEC='' #'--changeC'
-
+fi
 ### PSA64 Options ###
 
 #EVEN_FILES='/home/cacheng/capo/ctc/matt_data/lstbin_psa64_data_optimal/even/*uvGAL'
@@ -38,11 +62,27 @@ mkdir ${DIRNAME}
 echo Making Directory ${DIRNAME}
 
 # Stage 1: pspec_oqe_2d.py over range of injection levels
-for inject in `python -c "import numpy; print ' '.join(map(str, numpy.logspace(-2,3,10)))"` ; do
+for inject in `python -c "import numpy; print ' '.join(map(str, numpy.logspace(-2,3,1)))"` ; do
     mkdir ${DIRNAME}/inject_sep${SEP}_${inject}
     echo SIGNAL_LEVEL=${inject}
-    ~/capo/pspec_pipeline/pspec_oqe_2d.py ${LMODE} ${CHANGEC} --window=${WINDOW} -a cross -p ${POL} -c ${CHAN} -C ${CALFILE} -i ${inject} --weight=${weight} ${FRF} --output ${DIRNAME}/inject_sep${SEP}_${inject} ${EVEN_FILES} ${ODD_FILES}
+
+    #calculate error bars across 5 groups (the SUBSET)
+    ~/capo/pspec_pipeline/pspec_oqe_2d.py ${LMODE} ${CHANGEC} --window=${WINDOW} -a cross -p ${POL} -c ${CHAN} \
+    -C ${CALFILE} -i ${inject} --weight=${weight} ${FRF} --output ${DIRNAME}/inject_sep${SEP}_${inject} \
+    ${EVEN_FILES} ${ODD_FILES} --NGPS=5
+
+    #get the deepest power spectrum (the FULL)
+    ~/capo/pspec_pipeline/pspec_oqe_2d.py ${LMODE} ${CHANGEC} --window=${WINDOW} -a cross -p ${POL} -c ${CHAN} \
+    -C ${CALFILE} -i ${inject} --weight=${weight} ${FRF} --output ${DIRNAME}/inject_sep${SEP}_${inject} \
+    ${EVEN_FILES} ${ODD_FILES} --NGPS=1
 
     # Stage 2: pspec_2d_to_1d.py
-    ~/capo/pspec_pipeline/pspec_2d_to_1d.py ${FRF} ${NOFRFPATH} --output ${DIRNAME}/inject_sep${SEP}_${inject}/ ${DIRNAME}/inject_sep${SEP}_${inject}/*pspec_oqe_2d*
+    #(variance across the subset)
+    ~/capo/pspec_pipeline/pspec_2d_to_1d.py ${FRF} ${NOFRFPATH} \
+    --output=${DIRNAME}/inject_sep${SEP}_${inject}/ ${DIRNAME}/inject_sep${SEP}_${inject}/pspec_oqe_2d.npz
+    
+    #average over bls and lsts (todo: remove unnecessary bootstrap)
+    ~/capo/pspec_pipeline/pspec_2d_to_1d.py ${FRF} ${NOFRFPATH} \
+    --output=${DIRNAME}/inject_sep${SEP}_${inject}/ ${DIRNAME}/inject_sep${SEP}_${inject}/pspec_oqe_2d_final.npz
+
 done
