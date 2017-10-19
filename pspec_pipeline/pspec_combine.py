389b16e3ab0abbc6ece395e22581c41cc7ef0e5e
Carina Cheng <cacheng@folio.sas.upenn.edu>
2017-10-19 15:13:36 -0400

    -Added some print statements -Overwrites 'sep' key to contain all seps used -Adds 'nseps' key for number of separations

--- pspec_pipeline/pspec_combine.py
+++ pspec_pipeline/pspec_combine.py
@@ -30,13 +30,16 @@ pCn_fold_err = {}
 pIn_fold = {}
 pIn_fold_err = {}
 kperps = []
+seps = []
 
 # Read in PS for different seps
 for file in args.files: # XXX only compatible with pspec_pk_k3pk.npz files 
+    print 'Reading', file
     f = n.load(file)
     metakeys = [key for key in f.keys() if not (key.startswith('p') or key.startswith('k'))] # all keys except PS values and k,kpl,kpl_fold,kperp
     kperps.append(f['kperp'])
-    for kk, k in enumerate(f['kpl']):
+    seps.append(f['sep'])
+    for kk, k in enumerate(f['kpl']): # each dictionary has keys of k's and contains a list of values for that k (3 if there are 3 seps)
         pCv.setdefault(k,[]).append(f['pCv'][kk])
         pCv_err.setdefault(k,[]).append(f['pCv_err'][kk])
         pIv.setdefault(k,[]).append(f['pIv'][kk])
@@ -71,6 +74,7 @@ def combine_PS(ks, vals, errs):
         err_values.append(n.sqrt(err_value)) # back to std
     return values, err_values
 
+print 'Using', args.weight, 'weighting'
 pCv_combine, pCv_err_combine = combine_PS(f['kpl'], pCv, pCv_err)
 pIv_combine, pIv_err_combine = combine_PS(f['kpl'], pIv, pIv_err)
 pCv_fold_combine, pCv_fold_err_combine = combine_PS(f['kpl_fold'], pCv_fold, pCv_fold_err)
@@ -90,9 +94,9 @@ final_file = 'pspec_final_combine.npz'
 metadata = {}
 for metakey in metakeys: metadata[metakey] = f[metakey]
 metadata['cmd'] = metadata['cmd'].item() + '\n ' + ' '.join(sys.argv)
+metadata['sep'] = seps
 
-# TO DO: over-write seps if wanted
-
+print 'Saving', final_file
 n.savez(final_file, k=k, kperp=kperp, kpl=kpl, kpl_fold=kpl_fold,
         pCv=pCv_combine, pCv_err=pCv_err_combine,
         pIv=pIv_combine, pIv_err=pIv_err_combine, 
@@ -102,7 +106,7 @@ n.savez(final_file, k=k, kperp=kperp, kpl=kpl, kpl_fold=kpl_fold,
         pIv_fold=pIv_fold_combine, pIv_fold_err=pIv_fold_err_combine, 
         pCn_fold=pCn_fold_combine, pCn_fold_err=pCn_fold_err_combine, 
         pIn_fold=pIn_fold_combine, pIn_fold_err=pIn_fold_err_combine, 
-        **metadata)
+        nseps=len(pCv[pCv.keys()[0]]), **metadata)
 
 
 
