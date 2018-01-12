0ba3b88e184095482cca20e7b7beace84db81eab
Carina Cheng <cacheng@folio.sas.upenn.edu>
2018-01-12 14:36:40 -0500

    dotted 1:1 line

--- ctc/code/plot_sigloss.py
+++ ctc/code/plot_sigloss.py
@@ -56,7 +56,7 @@ plt.subplot(121)
 #plt.plot(n.abs(Pins.flatten()),n.abs(Pouts.flatten()),'k.',label="All bootstraps")
 #plt.plot(10**bins,10**n.polyval(coeff,bins),'r-',label="Polynomial Fit")
 plt.pcolormesh(10**bins,10**bins,M,cmap='hot_r')
-plt.plot([pklo, pkhi], [pklo, pkhi], 'k-')  # diagonal line
+plt.plot([pklo, pkhi], [pklo, pkhi], 'k:')  # diagonal line
 plt.hlines(y=n.abs(file_pts[k_pts_ind]),xmin=pklo,xmax=pkhi,color='0.5',linewidth=3) # peak of original distribution
 #plt.legend(numpoints=1,prop={'size':10},loc='best')
 #plt.grid()
@@ -77,7 +77,7 @@ plt.subplot(122)
 #plt.plot(10**bins,10**n.polyval(coeff_I,bins),'r-',label="Polynomial Fit")
 plt.hlines(y=file_pts_I[k_pts_ind],xmin=pklo,xmax=pkhi,color='0.5',linewidth=3)
 plt.pcolormesh(10**bins,10**bins,M_I,cmap='hot_r')
-plt.plot([pklo, pkhi], [pklo, pkhi], 'k-')  # diagonal line
+plt.plot([pklo, pkhi], [pklo, pkhi], 'k:')  # diagonal line
 #plt.legend(numpoints=1,prop={'size':10},loc='best')
 #plt.grid()
 plt.yscale('log')
