0926e487d1b7ccfe571d7f0cc30e3774153d5834
Carina Cheng <cacheng@folio2.sas.upenn.edu>
2018-03-15 17:02:05 -0400

    saves other needed PS information too, like k's. And outputs new file in an inject directory structure

--- ctc/code/pspec_difference.py
+++ ctc/code/pspec_difference.py
@@ -3,6 +3,7 @@
 import numpy as n
 import optparse
 import sys
+import os
 
 o = optparse.OptionParser()
 opts, args = o.parse_args(sys.argv[1:])
@@ -17,14 +18,16 @@ print "Loading", args[0]
 f1 = n.load(args[0])
 print"Loading", args[1]
 f2 = n.load(args[1])
-fname = args[0].split('/')[-1] # name of outputted file
+os.system('mkdir '+args[0].split('/')[-2]) # make inject directory
+fname = args[0].split('/')[-2]+'/'+args[0].split('/')[-1] # name of outputted file
 fnew = {}
 
 # Difference keys
 for key in f1:
     if key[0] == 'p': # PS channel
         fnew[key] = f1[key] - f2[key]        
-
+    else:
+        fnew[key] = f1[key] # copy other channels
 # Save new file
 n.savez(fname, **fnew)
-print "   Saving", fname, "in current directory"
+print "   Saving", fname
