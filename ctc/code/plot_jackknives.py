b53e6a5e4734da607943147fc8e9893d6c3ecdf2
Carina Cheng <cacheng@folio2.sas.upenn.edu>
2018-03-20 15:11:47 -0400

    plots all 3 null tests as PS differences

--- ctc/code/plot_jackknives.py
+++ ctc/code/plot_jackknives.py
@@ -3,6 +3,7 @@
 import numpy as n
 import pylab as p
 
+# Old jackknives ((e+o) x (e-o) for example)
 #file_eo = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_evenodd_addI/pspec_final_sep0,1.npz')
 #file_bl = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_baselines_addI/pspec_final_sep0,1.npz')
 #file_lst = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_lst_addI/pspec_final_sep0,1.npz')
@@ -11,15 +12,21 @@ file_bl = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_baselines_addI/
 file_lst = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_lst_addI/pspec_final_sep0,1_nosigloss.npz')
 data = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/rangeofaddedidentity_trace_log_DENSE_NOSEED/add_0.0206913808111_identity/pspec_final_sep0,1.npz')
 
+
+# New jackknives (differencing of 2 PS)
 file_lst_diff = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_lst/pspec_final_sep0,1_full.npz')
 file_lst_1 = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_lst/lst_1/pspec_final_sep0,1_full.npz')
 file_lst_2 = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_lst/lst_2/pspec_final_sep0,1_full.npz')
 file_bls_diff = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_bls/pspec_final_sep0,1_full.npz')
 file_bls_1 = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_bls/bls_1/pspec_final_sep0,1_full.npz')
 file_bls_2 = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_bls/bls_2/pspec_final_sep0,1_full.npz')
+file_eo_diff = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_evenodd/pspec_final_sep0,1_full.npz')
+file_eo_1 = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_evenodd/even/pspec_final_sep0,1_full.npz')
+file_eo_2 = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/jackknife_evenodd/odd/pspec_final_sep0,1_full.npz')
 
-noise = 4436767.36822 # XXX
+
 """
+noise = 4436767.36822 # XXX
 # Figure 1: zoom-out
 p.figure()
 p.errorbar(file_eo['kpl'],file_eo['pCv'],file_eo['pCv_err']*2,linestyle='',marker='.',color='b',label='Even/Odd Null Test')
@@ -43,12 +50,12 @@ p.errorbar(file_lst_diff['kpl'],file_lst_diff['pIv_old'],file_lst_diff['pIv_err_
 #p.errorbar(file_lst_1['kpl']-0.005,file_lst_1['pIv_old'],file_lst_1['pIv_err_old']*2,linestyle='',marker='.',color='k',label='LST1')
 #p.errorbar(file_lst_2['kpl']+0.005,file_lst_2['pIv_old'],file_lst_2['pIv_err_old']*2,linestyle='',marker='.',color='0.5',label='LST2')
 p.axhline(0,color='k',linestyle='--')
-p.axhspan(-noise*2,noise*2,facecolor='0.5',edgecolor="none",alpha=0.5,label='Estimated $2\sigma$ Error')
+p.axhspan(-noise_lst*2,noise_lst*2,facecolor='0.5',edgecolor="none",alpha=0.5,label='Estimated $2\sigma$ Error')
 #p.legend(numpoints=1,prop={'size':16})
 p.ylabel('$P(k)$ $[mK^{2}(h^{-1} Mpc)^{3}]$',fontsize=18)
 p.xlabel('$k_{\\parallel}$ [$h$ Mpc$^{-1}$]',fontsize=18)
 p.tick_params(axis='both', which='major', labelsize=16)
-p.ylim(-1.6e9,1.7e9)
+p.ylim(-1.2e9,1.2e9)
 p.grid()
 p.title('Null Test: LST',fontsize=18)
 
@@ -64,9 +71,25 @@ p.axhspan(-noise_bl*2,noise_bl*2,facecolor='0.5',edgecolor="none",alpha=0.5,labe
 #p.ylabel('$P(k)$ $[mK^{2}(h^{-1} Mpc)^{3}]$',fontsize=18)
 p.xlabel('$k_{\\parallel}$ [$h$ Mpc$^{-1}$]',fontsize=18)
 p.tick_params(axis='both', which='major', labelsize=16)
-p.ylim(-1.6e9,1.7e9)
+p.ylim(-1.2e9,1.2e9)
 p.grid()
 p.title('Null Test: Baselines',fontsize=18)
+
+# Even/Odd null test
+noise_eo = 4436767.36822*2 # sensitivity of usual even/odd multiplied by 2, since now we're only doing one quadrant of the grid instead of 2
+p.figure()
+p.errorbar(file_eo_diff['kpl'],file_eo_diff['pIv_old'],file_eo_diff['pIv_err_old']*2,linestyle='',marker='.',color='k')#,label='Even/Odd Null Test')
+#p.errorbar(file_eo_1['kpl']-0.005,file_eo_1['pIv_old'],file_eo_1['pIv_err_old']*2,linestyle='',marker='.',color='k',label='Even')
+#p.errorbar(file_eo_2['kpl']+0.005,file_eo_2['pIv_old'],file_eo_2['pIv_err_old']*2,linestyle='',marker='.',color='0.5',label='Odd')
+p.axhline(0,color='k',linestyle='--')
+p.axhspan(-noise_eo*2,noise_eo*2,facecolor='0.5',edgecolor="none",alpha=0.5,label='Estimated $2\sigma$ Error')
+#p.legend(numpoints=1,prop={'size':16})
+#p.ylabel('$P(k)$ $[mK^{2}(h^{-1} Mpc)^{3}]$',fontsize=18)
+p.xlabel('$k_{\\parallel}$ [$h$ Mpc$^{-1}$]',fontsize=18)
+p.tick_params(axis='both', which='major', labelsize=16)
+p.ylim(-1.2e9,1.2e9)
+p.grid()
+p.title('Null Test: Even/Odd',fontsize=18)
 
 p.show()
 
