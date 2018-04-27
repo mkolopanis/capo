2dc976c74fd1639bc6907d9c5b97f4f2888bbae3
Carina Cheng <cacheng@folio2.sas.upenn.edu>
2018-04-27 17:01:09 -0400

    plots upper limits instead of just errors

--- ctc/code/plot_bootmethods.py
+++ ctc/code/plot_bootmethods.py
@@ -13,8 +13,8 @@ boot_time = n.load('/data4/paper/ctc/PSA64/PAPER_METHODS/compare_boot_methods/ps
 noise = 4436767.36822 # XXX
 
 # Plot
-plt.plot(boot_bl['kpl'], boot_bl['pIn_err_old']*2, color='black', linewidth=2, label='Bootstrap baselines only')
-plt.plot(boot_time['kpl'], boot_time['pIn_err_old']*2, color='0.5', linewidth=2, label='Bootstrap baselines and times')
+plt.plot(boot_bl['kpl'], n.abs(boot_bl['pIn_old'])+boot_bl['pIn_err_old']*2, color='black', linewidth=2, label='Bootstrap baselines only')
+plt.plot(boot_time['kpl'], n.abs(boot_time['pIn_old'])+boot_time['pIn_err_old']*2, color='0.5', linewidth=2, label='Bootstrap baselines and times')
 plt.axhline(y=noise*2, color='green', linestyle='-', linewidth=2, label='Analytic')
 plt.legend(numpoints=1,prop={'size':14},loc='best')
 plt.grid()
@@ -24,7 +24,7 @@ plt.xlabel('$k_{\\parallel}$ [$h$ Mpc$^{-1}$]',fontsize=18)
 plt.ylabel('$P(k)$ $[mK^{2}(h^{-1} Mpc)^{3}]$',fontsize=18)
 plt.tick_params(axis='both', which='major', labelsize=14)
 #plt.ylim(1e6,7e9) # for data
-plt.ylim(5e5,1e8)
+plt.ylim(1e6,1e8)
 plt.show()
 
 
